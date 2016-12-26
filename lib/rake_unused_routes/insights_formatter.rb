e6dae50ee50f12a865f6e4577cffba08cf3a13fa
Karl Baum <karl.baum@gmail.com>
2016-12-26 11:04:13 -0500

    introduced actions from insights method

--- lib/rake_unused_routes/insights_formatter.rb
+++ lib/rake_unused_routes/insights_formatter.rb
@@ -5,22 +5,25 @@ class RakeUnusedRoutes
     class << self
 
       def convert_from_insights!
-        actions = CSV.read(insights_csv_file_path).
+        csv_file = CSV.generate do |csv|
+          csv << [ 'Action' ]
+          actions_from_insights.each{ |action| csv << [ action ] }
+        end
+        FileUtils.mkdir_p tmp_dir_path
+        File.open("#{tmp_dir_path}/controller_summary.csv", 'w+') do  |file|
+          file.write csv_file
+        end
+      end
+
+      private
+
+      def actions_from_insights
+        CSV.read(insights_csv_file_path).
           drop(1).map(&:first).reject{|r| r=~/Middleware/ }.
           map{ |r| r.sub('Controller/', '') }.
           map{|r| "#{r.split('/')[0..-2].
           map{|r| r.camelize}.join('::')}Controller##{r.split('/').last}" }.sort
-          csv_file = CSV.generate do |csv|
-            csv << [ 'Action' ]
-            actions.each{ |action| csv << [ action ] }
-          end
-          FileUtils.mkdir_p tmp_dir_path
-          File.open("#{tmp_dir_path}/controller_summary.csv", 'w+') do  |file|
-            file.write csv_file
-          end
       end
-
-      private
 
       def tmp_dir_path
         './tmp'
